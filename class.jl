de8346cee05e0cc866f6b460da361dccd339550e
Alexandre Heiden <alexandreheiden@hotmail.com>
2018-09-25 14:51:35 -0300

    ackley

--- class.jl
+++ class.jl
@@ -1,11 +1,13 @@
 mutable struct ga
 	cx
 	mr
+	lb
+	ub
 	elitist
 	population
 	next_population
 	fitness # every fitness
 	total_fitness
-	ga(pp) = new(0.96, 0.03, [], pp, [], [], 0)
-	ga(cx, mr, pp) = new(cx, mr, [], pp, [], [], 0)
+	ga(lb, ub, pp) = new(0.96, 0.03, [], lb, ub, pp, [], [], 0)
+	ga(cx, mr, lb, ub, pp) = new(cx, mr, [], lb, ub, pp, [], [], 0)
 end
