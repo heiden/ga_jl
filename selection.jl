8fc410d4f57871c52e688f1210849f2635797cab
Alexandre Heiden <alexandreheiden@hotmail.com>
2018-09-25 20:52:42 -0300

    added tourney

---
+++ selection.jl
@@ -0,0 +1,16 @@
+include("class.jl")
+
+function tourney(solver::ga, x)
+	selected = []
+	rng = MersenneTwisters.MT19937()
+	for i in 1:length(solver.population) / 2
+		picks = sample(rng, 1:length(solver.population), x)
+		candidates = [(solver.fitness[i], i) for i in picks]
+		sort!(candidates)
+		# if min
+		s = Pair(candidates[1][2], candidates[2][2])
+		# else Pair(candidates[length(candidates) - 1][2], candidates[length(candidates)][2])
+		push!(selected, s)
+	end
+	return selected
+end
