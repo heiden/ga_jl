88eb122a59ace3518252f84cd723ab758f70c195
Alexandre Heiden <alexandreheiden@hotmail.com>
2018-09-27 00:45:22 -0300

    added blx crossover

--- crossover.jl
+++ crossover.jl
@@ -3,11 +3,16 @@ include("class.jl")
 function one_point_crossover(solver::ga, selected)
 	rng = MersenneTwisters.MT19937()
 	for s in selected
-		oa, ob = [], []
 		ia, ib = solver.population[s[1]], solver.population[s[2]]
-		p = rand(rng, 1:length(ia))
-		append!(oa, ia[1:p]), append!(oa, ib[p+1:length(ib)])
-		append!(ob, ib[1:p]), append!(ob, ia[p+1:length(ia)])
+		ω = rand()
+		if ω < solver.cx
+			oa, ob = [], []
+			p = rand(rng, 1:length(ia))
+			append!(oa, ia[1:p]), append!(oa, ib[p+1:length(ib)])
+			append!(ob, ib[1:p]), append!(ob, ia[p+1:length(ia)])
+		else
+			oa, ob = ia, ib
+		end
 		push!(solver.next_population, oa), push!(solver.next_population, ob)
 	end
 end
@@ -15,11 +20,43 @@ end
 function two_point_crossover(solver::ga, selected)
 	rng = MersenneTwisters.MT19937()
 	for s in selected
-		oa, ob = [], []
 		ia, ib = solver.population[s[1]], solver.population[s[2]]
-		px, py = sort!(rand(rng, 1:length(ia), 2))
-		append!(oa, ia[1:px-1]), append!(oa, ib[px:py]), append!(oa, ia[py+1:length(ia)])
-		append!(ob, ib[1:px-1]), append!(ob, ia[px:py]), append!(ob, ib[py+1:length(ib)])
+		ω = rand()
+		if ω < solver.cx
+			oa, ob = [], []
+			px, py = sort!(rand(rng, 1:length(ia), 2))
+			append!(oa, ia[1:px-1]), append!(oa, ib[px:py]), append!(oa, ia[py+1:length(ia)])
+			append!(ob, ib[1:px-1]), append!(ob, ia[px:py]), append!(ob, ib[py+1:length(ib)])
+		else
+			oa, ob = ia, ib
+		end
+		push!(solver.next_population, oa), push!(solver.next_population, ob)
+	end
+end
+
+function blx(solver::ga, selected)
+	α = 0.5 # standard
+	for s in selected
+		ia, ib = solver.population[s[1]], solver.population[s[2]]
+		ω = rand()
+		if ω < solver.cx
+			oa, ob = [], []
+			for i in 1:length(ia)
+				Δ = abs(ia[i] - ib[i])
+				lb = max(solver.lb, min(ia[i], ib[i]) - α*Δ)
+				ub = min(solver.ub, max(ia[i], ib[i]) + α*Δ)
+				if Δ != 0 
+					d = Uniform(lb, ub)
+					x = rand(d)
+				else
+					x = lb
+				end
+				append!(oa, x)
+				append!(ob, x)
+			end
+		else
+			oa, ob = ia, ib
+		end
 		push!(solver.next_population, oa), push!(solver.next_population, ob)
 	end
 end
